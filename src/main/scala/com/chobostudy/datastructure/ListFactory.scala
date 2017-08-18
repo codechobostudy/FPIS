3ed30e6e852c1ffd04626fda38b8c19eafe06a96
loustler <dev.loustler@gmail.com>
2017-08-18 20:15:11 +0900

    Add provided example.

--- src/main/scala/com/chobostudy/datastructure/ListFactory.scala
+++ src/main/scala/com/chobostudy/datastructure/ListFactory.scala
@@ -22,4 +22,13 @@ abstract class ListFactory extends ObjectFactory[List] with ListHelper {
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
   }
+
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = as match {
+    case Nil        => z
+    case Cons(h, t) => f(h, foldRight(t, z)(f))
+  }
+
+  def sum2(ns: List[Int]) = foldRight(ns, 0)((x, y) => x + y)
+
+  def product2(ns: List[Double]) = foldRight(ns, 1.0)(_ * _)
 }
