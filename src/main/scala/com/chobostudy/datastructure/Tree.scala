c7674a2172f07cc77b287749c15b1733b6e47af5
jung myung sung <kkforgg@naver.com>
2017-09-07 11:44:32 +0900

    datastructure  conflict merge

--- src/main/scala/com/chobostudy/datastructure/Tree.scala
+++ src/main/scala/com/chobostudy/datastructure/Tree.scala
@@ -1,18 +1,5 @@
 package com.chobostudy.datastructure
-
-/**
-<<<<<<< HEAD
- * Don't edit this.
- *
- * If you want edit to extends it.
- *
- * @author audtjddld
- * @since 08/27/2017
- */
-trait Tree[+A]
-case class Leaf[A](value: A) extends Tree[A]
-case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
-=======
+ /*
   * @author loustler
   * @since 09/07/2017 00:01
   */
@@ -21,4 +8,3 @@ case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree
->>>>>>> 8d3ee3273e7ea91c93b79305e45f1fbd2c81fc23
